901e1f149ed288b60ed5ac28aaa2cff461b5a78d
syedjawadakhtar <syedjawadakhtar@gmail.com>
2025-12-03 12:08:49 +0200

    Add layout around routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import Layout from "@/components/Layout";
 import Dashboard from "@/components/Dashboard.tsx";
 import MedicationSummary from "@/components/MedicationSummary.tsx";
 import VisitSummary from "@/components/VisitSummary.tsx";
@@ -18,13 +17,15 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/patients/:slug/medication_summary" element={<MedicationSummary />} />
-          <Route path="/patients/:slug/visit_summary" element={<VisitSummary />} />
-          <Route path="/patients/:slug/referral" element={<ReferralGeneration />} />
-        </Routes>
+        <Layout>
+          <Routes>
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/patients/:slug/medication_summary" element={<MedicationSummary />} />
+            <Route path="/patients/:slug/visit_summary" element={<VisitSummary />} />
+            <Route path="/patients/:slug/referral" element={<ReferralGeneration />} />
+          </Routes>
+        </Layout>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
